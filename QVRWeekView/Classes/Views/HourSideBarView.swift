0f293d87ac80e8e2a9e2571587dcaaab39f2878d
Manan Devani <manan.devani@biz4solutions.com>
2021-01-07 12:25:06 +0530

    extension color added for random color.

--- QVRWeekView/Classes/Views/HourSideBarView.swift
+++ QVRWeekView/Classes/Views/HourSideBarView.swift
@@ -89,3 +89,20 @@ class HourSideBarView: UIView {
     }
 
 }
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(
+           red: .random(),
+           green: .random(),
+           blue: .random(),
+           alpha: 1.0
+        )
+    }
+}
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
