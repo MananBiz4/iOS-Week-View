e1b0850d80c3f3f9465d8c7f543631fd05f96812
Manan Devani <manan.devani@biz4solutions.com>
2021-01-07 12:23:57 +0530

    enum file added - hour label position.

---
+++ QVRWeekView/Classes/Common/HourLabelPosition.swift
@@ -0,0 +1,20 @@
+//
+//  HourLabelPosition.swift
+//  Pods
+//
+//  Created by Manan on 07/01/21.
+//
+
+import Foundation
+
+/**
+ Enum is used to determine position of hour label.
+ */
+public enum HourLabelPosition {
+    // Show hour at the top of cell.
+    case top
+    // Show hour at the middle of cell.
+    case middle
+    // Show hour at the bottom of cell.
+    case bottom
+}
