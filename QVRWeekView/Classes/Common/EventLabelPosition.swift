a41e3ddc5bd6ccfe88a296339dbccd4e52c20860
Manan Devani <manan.devani@biz4solutions.com>
2021-01-08 16:54:19 +0530

    enum file created - event label position

---
+++ QVRWeekView/Classes/Common/EventLabelPosition.swift
@@ -0,0 +1,18 @@
+//
+//  EventLabelPosition.swift
+//  Pods
+//
+//  Created by Manan on 08/01/21.
+//
+
+import Foundation
+
+/**
+ Enum is used to determine position of hour label.
+ */
+public enum EventLabelPosition {
+    // Show hour at the top of cell.
+    case top
+    // Show hour at the middle of cell.
+    case middle
+}
